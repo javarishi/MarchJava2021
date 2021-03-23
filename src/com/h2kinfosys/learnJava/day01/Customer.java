827e1d993cbf91c29e7c771c24b247651ca02596
RISHI <Rishi rishi.h2kinfosys@gmail.com>
2021-03-23 07:52:20 +0530

    Commit

---
+++ src/com/h2kinfosys/learnJava/day01/Customer.java
@@ -0,0 +1,41 @@
+package com.h2kinfosys.learnJava.day01;
+// class - is keyword to create Class
+public class Customer {
+
+	// Double slash is single line comment
+	/* 
+	 * This is Multi line comment in Java
+	 * As many lines you want
+	 */
+	// Variables - fields
+	// functions - methods
+	// Type - Size
+	// Integer - Numbers 
+	
+	
+	
+	public static void main(String[] args) {
+		// DataType variableName = value;
+		byte byteData = -128;
+		Byte byteObj = -127;
+		System.out.println(Byte.MIN_VALUE);
+		
+		short shortData = 32000;
+		int intData = 999999999;
+		long longData = 99999999999L;
+		
+		// Decimal Point 
+		float floatData= 3324.32432f;
+		double doubleData = 432234.234324d;
+		
+		// Character 
+		char singleChara = '@';
+		
+		// Boolean
+		boolean flag = false;
+		
+		// HW 1 - Try to find all Wrapper Classes
+		// HW 2 - Print Max and Min Values - rishi.h2kinfosys@gmail.com
+	}
+	
+}
